846ff9e645aa7db209c7b19732b7ddbfae0bdfb7
Dan <dansantosalfa@gmail.com>
2023-06-08 19:06:17 -0300

    Add Entrada dos dados

--- JurosDividas.js
+++ JurosDividas.js
@@ -1,1 +1,7 @@
 import  entrada from 'readline-sync';
+
+//Entrada de dados
+let valor = entrada.question("Informe o valor devido: ");
+let dias_passados = entrada.question("Informe quantos dias se passaram desde o vencimento do boleto: ");
+let taxa_juros = 10;
+
