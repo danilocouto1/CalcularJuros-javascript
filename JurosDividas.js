2c6dade49642b9c0d692fe99638c1fd255f5e82f
Dan <dansantosalfa@gmail.com>
2023-06-08 19:25:38 -0300

    Add - Processamento do valor final

--- JurosDividas.js
+++ JurosDividas.js
@@ -4,13 +4,18 @@ import  entrada from 'readline-sync';
 let valor = entrada.question("Informe o valor devido: ");
 let dias_passados = entrada.question("Informe quantos dias se passaram desde o vencimento do boleto: ");
 let taxa_juros;
+let valor_juros;
 
 //Processamento de dados
 if (valor > 0) {
     if (dias_passados <= 15 && dias_passados > 0) {
         taxa_juros = 5;
+
+        valor_juros = valor + ((valor / 100) * taxa_juros);
     } else if (dias_passados > 15) {
         taxa_juros = 10;
+
+        valor_juros = valor + ((valor / 100) * taxa_juros);
     } {
         console.log("Você esta em dia!")
     }
